6ac5a5e25cc108765478b13831e21166ac68055a
BlueCloud02 <comptes.98@gmail.com>
2019-03-07 20:19:37 +0100

    model part: create entities, do doctrine migration

---
+++ src/Entity/Product.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(name="product_id", type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=10, unique=true)
+     */
+    private $reference;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $saleNoticeDate;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     */
+    private $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(string $reference): self
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getSaleNoticeDate(): ?\DateTimeInterface
+    {
+        return $this->saleNoticeDate;
+    }
+
+    public function setSaleNoticeDate(?\DateTimeInterface $saleNoticeDate): self
+    {
+        $this->saleNoticeDate = $saleNoticeDate;
+
+        return $this;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
