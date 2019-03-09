cf597df1c24d5add33d45b8559979d6432a7c6f0
BlueCloud02 <comptes.98@gmail.com>
2019-03-09 12:09:25 +0100

    Install Encore and front-end libraries. Integration of the store page

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Brand;
+use App\Entity\Product;
 
 class StoreController extends AbstractController {
 	/**
@@ -11,7 +13,21 @@ class StoreController extends AbstractController {
      * @Route("/store", name="store_index")
      */
 	public function indexAction() {
-		return $this->render('store/index.html.twig');
-	}
+		$em = $this->getDoctrine()->getManager();
+        /** @var ProductRepository $productRepo */
+        $productRepo = $em->getRepository(Product::class);
+        /** @var BrandRepository $brandRepo */
+        $brandRepo = $em->getRepository(Brand::class);
+
+        /** @var Product[] $products */
+        $products = $productRepo->findAll();
+        /** @var Brand[] $brands */
+        $brands = $brandRepo->findAll();
+        
+        return $this->render('store/index.html.twig', [
+            'products' => $products,
+            'brands' => $brands
+        ]);
+    }
 }
 ?>
